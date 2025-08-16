78aff4f6e0ee3d9b26c76ce0567cc6d8eb16dcb7
frijoles4me <36173111+frijoles4me@users.noreply.github.com>
2025-08-16 13:26:43 -0700

    Add files via upload
    mini game

---
+++ minigame.py
@@ -0,0 +1,28 @@
+# Mini Game Project
+# RPG Game where player chooses there actions
+moves = True
+
+    
+while moves == True:
+     print('Player 1')
+     print('Select a move: 1) Punsh. 2) Slash. 3) Kick. 4) Heal.')
+     action = (int(input("What action are you taking.")))
+     if action == 1:
+        print("PUNCH!!!")
+     elif action == 2:
+        print("SLASH!!")
+     elif action == 3:
+         print("KICK!!!")
+     elif action == 4:
+         print("HEAL!!!")
+     else:
+        print('Type a from number 1 - 4')
+     
+
+
+
+
+print(moves)
+    
+ 
+    
